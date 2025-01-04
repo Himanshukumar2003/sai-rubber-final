8d96f587aaa015064c49ebf5e62e7749f5b3a4f5
himanshu <devliyalhimanshu@gmail.com>
2025-01-04 11:11:20 +0530

    first commit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-document.querySelectorAll('.dropdown-submenu a').forEach(function (submenu) {
-    submenu.addEventListener('click', function (event) {
-        var nextEl = submenu.nextElementSibling;
-        if (nextEl && nextEl.classList.contains('dropdown-menu')) {
-            event.preventDefault();
-            nextEl.classList.toggle('show');
-        }
-    });
-});
-
-
 
 
 const counterAnimation = () => {
